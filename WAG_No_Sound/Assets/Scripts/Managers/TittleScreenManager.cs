c5909418c499b66d1c848dfcdcd9c81ed096fc4d
Sebi-Lopez <sebi8818@gmail.com>
2020-05-14 18:50:53 +0200

    1.1 - Hero Goal and God Goal - Pre-entry and loop

---
+++ WAG_No_Sound/Assets/Scripts/Managers/TittleScreenManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TittleScreenManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audio_init;
+    [SerializeField]
+    private AudioSource audio_loop;
+
+    void Start()
+    {
+        audio_init.loop = false;
+        audio_init.Play();
+
+        audio_loop.loop = true;
+        audio_loop.PlayDelayed(audio_init.clip.length);
+    }
+
+}
